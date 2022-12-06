a7222706317b17cdd20220703d1ceaaf919b740c
Heikki Hannikainen <hessu@hes.iki.fi>
2022-12-06 22:52:27 +0000

    Improvements
    use paho-mqtt, monotonic time, fix reconnects, python3.

--- mqtt-bme280.py
+++ mqtt-bme280.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: iso-8859-1 -*-
 
 __author__ = "Kyle Gordon, Heikki Hannikainen"
@@ -11,14 +11,14 @@ import socket
 import time
 import sys
 
-import mosquitto
-import ConfigParser
+import paho.mqtt.client as mqtt
+import configparser
 import setproctitle
 
 from Adafruit_BME280 import *
 
 # Read the config file
-config = ConfigParser.RawConfigParser()
+config = configparser.RawConfigParser()
 config.read("mqtt-bme280.cfg")
 
 # Use ConfigParser to pick out the settings
@@ -36,7 +36,7 @@ APPNAME = "mqtt-bme280"
 PRESENCETOPIC = "clients/" + socket.getfqdn() + "/" + APPNAME + "/state"
 setproctitle.setproctitle(APPNAME)
 client_id = APPNAME + "_%d" % os.getpid()
-mqttc = mosquitto.Mosquitto(client_id)
+mqttc = mqtt.Client(client_id=client_id)
 
 LOGFORMAT = '%(asctime)-15s %(message)s'
 
@@ -53,102 +53,24 @@ logging.info("Starting " + APPNAME)
 logging.info("INFO MODE")
 logging.debug("DEBUG MODE")
 
-# All the MQTT callbacks start here
+connected = False
 
-def on_publish(mosq, obj, mid):
-    """
-    What to do when a message is published
-    """
-    logging.debug("MID " + str(mid) + " published.")
+def on_mqtt_connect(client, userdata, flags, rc):
+    logging.debug("on_connect RC: " + str(rc))
+    logging.info("mqtt: Connected to %s:%s", MQTT_HOST, MQTT_PORT)
+    mqttc.publish(PRESENCETOPIC, "1", retain=True)
 
-
-def on_subscribe(mosq, obj, mid, qos_list):
-    """
-    What to do in the event of subscribing to a topic"
-    """
-    logging.debug("Subscribe with mid " + str(mid) + " received.")
-
-
-def on_unsubscribe(mosq, obj, mid):
-    """
-    What to do in the event of unsubscribing from a topic
-    """
-    logging.debug("Unsubscribe with mid " + str(mid) + " received.")
-
-
-def on_connect(mosq, obj, result_code):
-    """
-    Handle connections (or failures) to the broker.
-    This is called after the client has received a CONNACK message
-    from the broker in response to calling connect().
-    The parameter rc is an integer giving the return code:
-
-    0: Success
-    1: Refused – unacceptable protocol version
-    2: Refused – identifier rejected
-    3: Refused – server unavailable
-    4: Refused – bad user name or password (MQTT v3.1 broker only)
-    5: Refused – not authorised (MQTT v3.1 broker only)
-    """
-    logging.debug("on_connect RC: " + str(result_code))
-    if result_code == 0:
-        logging.info("Connected to %s:%s", MQTT_HOST, MQTT_PORT)
-        # Publish retained LWT as per
-        # http://stackoverflow.com/q/97694
-        # See also the will_set function in connect() below
-        mqttc.publish(PRESENCETOPIC, "1", retain=True)
-        process_connection()
-    elif result_code == 1:
-        logging.info("Connection refused - unacceptable protocol version")
-        cleanup()
-    elif result_code == 2:
-        logging.info("Connection refused - identifier rejected")
-        cleanup()
-    elif result_code == 3:
-        logging.info("Connection refused - server unavailable")
-        logging.info("Retrying in 30 seconds")
-        time.sleep(30)
-    elif result_code == 4:
-        logging.info("Connection refused - bad user name or password")
-        cleanup()
-    elif result_code == 5:
-        logging.info("Connection refused - not authorised")
-        cleanup()
+def on_mqtt_disconnect(self, client, userdata, rc=0):
+    logging.info("on_mqtt_disconnect")
+    #global connected
+    #connected = False
+    if rc == 0:
+        logging.info("mqtt: Clean disconnection")
     else:
-        logging.warning("Something went wrong. RC:" + str(result_code))
-        cleanup()
-
-
-def on_disconnect(mosq, obj, result_code):
-    """
-    Handle disconnections from the broker
-    """
-    if result_code == 0:
-        logging.info("Clean disconnection")
-        sys.exit(0)
-    else:
-        logging.info("Unexpected disconnection! Reconnecting in 5 seconds")
-        logging.debug("Result code: %s", result_code)
+        logging.info("mqtt: Unexpected disconnection! Reconnecting in 5 seconds")
+        logging.debug("Result code: %s", rc)
         time.sleep(5)
 
-
-def on_message(mosq, obj, msg):
-    """
-    What to do when the client recieves a message from the broker
-    """
-    logging.debug("Received: " + msg.payload +
-                  " received on topic " + msg.topic +
-                  " with QoS " + str(msg.qos))
-    process_message(msg)
-
-
-def on_log(mosq, obj, level, string):
-    """
-    What to do with debug log output from the MQTT library
-    """
-    logging.debug(string)
-
-# End of MQTT callbacks
 
 am_stopping = False
 
@@ -168,47 +90,22 @@ def cleanup(signum, frame):
     logging.info("Exiting on signal %d", signum)
     sys.exit(signum)
 
-def connect():
-    """
-    Connect to the broker, define the callbacks, and subscribe
-    This will also set the Last Will and Testament (LWT)
-    The LWT will be published in the event of an unclean or
-    unexpected disconnection.
-    """
-    logging.debug("Connecting to %s:%s", MQTT_HOST, MQTT_PORT)
-    # Set the Last Will and Testament (LWT) *before* connecting
-    mqttc.will_set(PRESENCETOPIC, "0", qos=0, retain=True)
-    result = mqttc.connect(MQTT_HOST, MQTT_PORT, 60, True)
-    if result != 0:
-        logging.info("Connection failed with error code %s. Retrying", result)
-        time.sleep(10)
-        connect()
-
-    # Define the callbacks
-    mqttc.on_connect = on_connect
-    mqttc.on_disconnect = on_disconnect
-    mqttc.on_publish = on_publish
-    mqttc.on_subscribe = on_subscribe
-    mqttc.on_unsubscribe = on_unsubscribe
-    mqttc.on_message = on_message
-    if DEBUG:
-        mqttc.on_log = on_log
-
+def mqtt_setup():
     mqttc.loop_start()
 
-def process_connection():
-    """
-    What to do when a new connection is established
-    """
-    logging.debug("Processing connection")
-
-
-def process_message(mosq, obj, msg):
-    """
-    What to do with the message that's arrived
-    """
-    logging.debug("Received: %s", msg.topic)
-
+def check_connection():
+    # mqttc.is_connected() returns False at all times for some reason...
+    global connected
+    if not connected:
+        logging.info("Connecting to %s:%s", MQTT_HOST, MQTT_PORT)
+        try:
+            mqttc.on_connect = on_mqtt_connect
+            mqttc.on_disconnect = on_mqtt_disconnect
+            mqttc.connect(MQTT_HOST, MQTT_PORT, keepalive=30)
+            logging.info("connect() returned, connected to mqtt at %s:%s", MQTT_HOST, MQTT_PORT)
+            connected = True
+        except OSError as ex:
+            logging.warning("Failed to connect (will try again later): %s", ex)
 
 def find_in_sublists(lst, value):
     for sub_i, sublist in enumerate(lst):
@@ -262,6 +159,9 @@ class SampleStore:
     def deviation(self):
         return abs(self.max - self.min)
 
+
+
+
 def main_loop():
     """
     The main loop in which we stay connected to the broker
@@ -279,19 +179,20 @@ def main_loop():
         logging.info("opened sensor")
         buf = ''
         going = True
-        last_send = time.time()
+        last_send = time.monotonic()
         
         temp_store = SampleStore()
         press_store = SampleStore()
         hum_store = SampleStore()
         
         while going:
+            check_connection()
             try:
                 temperature = sensor.read_temperature()
                 pascals = sensor.read_pressure()
                 pressure = pascals / 100.0
                 humidity = sensor.read_humidity()
-            except Exception, e:
+            except Exception as e:
                 logging.error("Sensor reading exception: %r" % e)
                 going = False
                 break
@@ -302,7 +203,7 @@ def main_loop():
             press_store.add(pressure)
             hum_store.add(humidity)
             
-            now = time.time()
+            now = time.monotonic()
             if now - last_send >= SENDINTERVAL or now < last_send:
                 last_send = now
                 
@@ -348,8 +249,7 @@ def main_loop():
 signal.signal(signal.SIGTERM, cleanup)
 signal.signal(signal.SIGINT, cleanup)
 
-# Connect to the broker and enter the main loop
-connect()
+mqtt_setup()
 
 # Try to start the main loop
 try:
